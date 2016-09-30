53d6cb99dd971af1de8ea3130bb02a8cee791730
Lun <lun@Luns-Macbook.local>
2016-09-30 16:59:59 +0300

    Initial Commit

---
+++ BrainStormer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BrainStormer
+//
+//  Created by Lun on 2016/9/30.
+//  Copyright © 2016年 Lun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BrainStormer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BrainStormer
+//
+//  Created by Lun on 2016/9/30.
+//  Copyright © 2016年 Lun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
